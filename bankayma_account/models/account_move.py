2e7799815495fd5009bad7dc1a68dbd2964e0fb9
hunki <hunki@lothlorien>
2023-03-08 09:53:49 +0100

    [ADD] #107 don't create intercompany invoices for income
    and allow to pay immediately

--- bankayma_account/models/account_move.py
+++ bankayma_account/models/account_move.py
@@ -63,7 +63,11 @@ class AccountMove(models.Model):
     def action_post(self):
         """Enforce intercompany journal on intercompany invoices"""
         for this in self:
-            if this.is_invoice() and this._find_company_from_invoice_partner():
+            if (
+                this.is_invoice()
+                and this._find_company_from_invoice_partner()
+                and self.env.context.get("bankayma_force_intercompany_journal", True)
+            ):
                 if this.is_sale_document():
                     journal = this.company_id.intercompany_sale_journal_id
                 else:
@@ -80,8 +84,20 @@ class AccountMove(models.Model):
                 result = to_send.action_invoice_sent()
         return result
 
+    def _inter_company_create_invoice(self, dest_company):
+        """Allow skipping creation of intercompany invoice"""
+        if self.env.context.get("skip_intercompany_invoice"):
+            return
+        return super()._inter_company_create_invoice(dest_company)
+
     def _bankayma_invoice_child_income(
-        self, company_id=None, fraction=0.07, account_code="___10", post=False
+        self,
+        company_id=None,
+        fraction=0.07,
+        account_code="___10",
+        post=True,
+        invoice_journal_id=None,
+        payment_journal_id=None,
     ):
         """Create invoices for income of child companies"""
         company = (
@@ -89,6 +105,7 @@ class AccountMove(models.Model):
             and self.env["res.company"].browse(company_id)
             or self.env.company
         )
+        invoices = self.browse([])
         for child in company.child_ids:
             move_lines = (
                 self.env["account.move.line"]
@@ -108,16 +125,27 @@ class AccountMove(models.Model):
             account = self.env["account.account"].search(
                 [
                     ("code", "like", account_code),
+                    ("company_id", "=", company.id),
                 ],
                 limit=1,
             )
             if not account:
                 continue
             invoice_form = Form(
-                self.env["account.move"].with_context(default_move_type="out_invoice"),
+                self.env["account.move"]
+                .with_context(
+                    default_move_type="out_invoice",
+                    skip_intercompany_invoice=True,
+                    bankayma_force_intercompany_journal=False,
+                )
+                .with_company(company),
                 "account.view_move_form",
             )
             invoice_form.partner_id = child.partner_id
+            if invoice_journal_id:
+                invoice_form.journal_id = self.env["account.journal"].browse(
+                    invoice_journal_id
+                )
             with invoice_form.invoice_line_ids.new() as invoice_line:
                 invoice_line.account_id = account
                 invoice_line.price_unit = sum(move_lines.mapped("credit")) * fraction
@@ -127,3 +155,19 @@ class AccountMove(models.Model):
             )
             if post:
                 invoice.action_post()
+                if payment_journal_id:
+                    payment_form = Form(
+                        self.env["account.payment.register"]
+                        .with_context(
+                            active_id=invoice.id,
+                            active_ids=invoice.ids,
+                            active_model=invoice._name,
+                        )
+                        .with_company(company)
+                    )
+                    payment_form.journal_id = self.env["account.journal"].browse(
+                        payment_journal_id
+                    )
+                    payment_form.save().action_create_payments()
+            invoices += invoice
+        return invoices
