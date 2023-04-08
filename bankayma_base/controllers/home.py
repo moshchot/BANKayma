28bcaa4d1674dcbcf08b600a749d3b049e872a62
Holger Brunn <mail@hunki-enterprises.com>
2023-04-08 11:14:08 +0200

    [ADD] #113 frontend home action for backend users

---
+++ bankayma_base/controllers/home.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import http
+
+from odoo.addons.web.controllers.home import Home as _Home
+
+
+class Home(_Home):
+    def _login_redirect(self, uid, redirect=None):
+        """Override redirect if we're logging in a user with a custom redirect"""
+        user = http.request.env["res.users"].sudo().browse(uid)
+        if not redirect and user.login_redirect:
+            redirect = user.login_redirect
+        return super()._login_redirect(uid, redirect=redirect)
