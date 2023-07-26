40643f1dba98701e46bac93e90f79a232c59ffeb
Holger Brunn <mail@hunki-enterprises.com>
2023-07-26 15:22:13 +0200

    [ADD] #296 make company name translatable

---
+++ bankayma_base/models/res_company.py
@@ -0,0 +1,10 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    name = fields.Char(
+        translate=True,
+    )
