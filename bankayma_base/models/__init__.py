7b982ab6ed2280661f481acdf1797d5456db6f6e
Holger Brunn <mail@hunki-enterprises.com>
2023-08-23 18:31:20 +0200

    [ADD] support multi companies in website

--- bankayma_base/models/__init__.py
+++ bankayma_base/models/__init__.py
@@ -1,6 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import board_board
 from . import ir_module
+from . import ir_http
 from . import res_company
 from . import res_users
 from . import res_partner
