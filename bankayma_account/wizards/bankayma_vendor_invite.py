e65fbb756b5258c8d88eb52e3ea41ad6f698c57c
Holger Brunn <mail@hunki-enterprises.com>
2023-11-14 16:20:01 +0100

    [FIX] #383 take care that newly created vendors are in the right company

--- bankayma_account/wizards/bankayma_vendor_invite.py
+++ bankayma_account/wizards/bankayma_vendor_invite.py
@@ -1,7 +1,8 @@
 # Copyright 2023 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 
 
 class BankaymaVendorInvite(models.TransientModel):
@@ -12,6 +13,16 @@ class BankaymaVendorInvite(models.TransientModel):
         relation="bankayma_vendor_invite_ir_attachment_rel"
     )
     partner_ids = fields.Many2many(relation="bankayma_vendor_invite_res_partner_rel")
+
+    def default_get(self, fields):
+        if (
+            self.env["res.partner"]
+            .sudo()
+            .browse(self.env.context.get("active_ids", []))
+            .mapped("user_ids")
+        ):
+            raise UserError(_("This partner already has a user!"))
+        return super().default_get(fields)
 
     def _action_send_mail(self, auto_commit=False):
         partners = (
@@ -24,14 +35,10 @@ class BankaymaVendorInvite(models.TransientModel):
             {
                 "signup_group_ids": [
                     (6, 0, self.env.ref("bankayma_base.group_vendor").ids)
-                ]
-            }
-        )
-        partners.mapped("user_ids").write(
-            {
-                "groups_id": [(4, self.env.ref("bankayma_base.group_vendor").id)],
-                "company_ids": [(4, self.env.company.id)],
-                "company_id": self.env.company.id,
+                ],
+                "signup_company_ids": [
+                    (6, 0, self.env.company.ids),
+                ],
             }
         )
         return super()._action_send_mail(auto_commit=auto_commit)
