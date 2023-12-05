e8a37aee4b383a92b7699d2c4b5b25f1abe6ca6d
Holger Brunn <mail@hunki-enterprises.com>
2023-12-05 08:54:49 +0100

    [ADD] #393 allow to not push a payment to sumit in payment wizard

--- bankayma_account/models/__init__.py
+++ bankayma_account/models/__init__.py
@@ -4,7 +4,6 @@ from . import account_fiscal_position
 from . import account_journal
 from . import account_move
 from . import account_move_line
-from . import account_payment_register
 from . import account_payment_method
 from . import account_tax
 from . import account_tax_group
