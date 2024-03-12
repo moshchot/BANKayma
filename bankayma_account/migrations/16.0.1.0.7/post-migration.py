a56ebf7318dba8f8dd280766f8545b731aa905a0
Holger Brunn <mail@hunki-enterprises.com>
2024-03-12 16:43:55 +0100

    [ADD] #593 migration script to move accounting items

---
+++ bankayma_account/migrations/16.0.1.0.7/post-migration.py
@@ -0,0 +1,29 @@
+from openupgradelib.openupgrade import migrate
+
+
+@migrate()
+def migrate(env, version=None):
+    journals = env["account.journal"].search(
+        [
+            ("intercompany_purchase_company_id", "!=", False),
+        ]
+    )
+    for journal in journals:
+        account = env["account.account"].search(
+            [
+                ("company_id", "=", journal.company_id.id),
+                ("code", "=ilike", "2%11"),
+            ],
+            limit=1,
+        )
+        move_lines = env["account.move.line"].search(
+            [
+                ("journal_id", "=", journal.id),
+                ("account_id.code", "=ilike", "2%03"),
+            ]
+        )
+        if move_lines:
+            env.cr.execute(
+                "update account_move_line set account_id=%s where id in %s",
+                (account.id, tuple(move_lines.ids)),
+            )
