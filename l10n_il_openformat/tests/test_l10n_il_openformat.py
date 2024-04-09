b3ed36e91cdfbaa12bfc6abf6a7a3b8698dc5ba5
Holger Brunn <mail@hunki-enterprises.com>
2024-04-09 10:34:21 +0200

    fixup! [ADD] #96 l10n_il_openformat (unfinished)

--- l10n_il_openformat/tests/test_l10n_il_openformat.py
+++ l10n_il_openformat/tests/test_l10n_il_openformat.py
@@ -1,5 +1,3 @@
-from base64 import b64decode
-
 from odoo.tests.common import TransactionCase
 
 
@@ -7,4 +5,3 @@ class TestL10nIlOpenformat(TransactionCase):
     def test_export(self):
         wizard = self.env["l10n.il.openformat.export"].create({})
         wizard.button_export()
-        b64decode(wizard.export_file).decode("iso8859-8").splitlines()
