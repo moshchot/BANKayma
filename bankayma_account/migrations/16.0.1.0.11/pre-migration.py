14b1f69849253f00df2fc7bbe35720f83153222e
Holger Brunn <mail@hunki-enterprises.com>
2024-09-03 04:47:46 +0200

    [ADD] #697 migration script for transition to donation dependency

---
+++ bankayma_account/migrations/16.0.1.0.11/pre-migration.py
@@ -0,0 +1,19 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version=None):
+    """
+    Delete views created by this addon for transition to donation dependency
+    """
+    for xmlid in (
+        "view_account_move_form",
+        "view_account_move_tree_invoice",
+        "view_res_bank_form",
+        "view_partner_form",
+        "view_partner_property_form_account_fiscal_position_vat_check",
+        "view_partner_form_contract",
+    ):
+        record = env.ref("bankayma_account.%s" % xmlid, False)
+        if record:
+            record.unlink()
