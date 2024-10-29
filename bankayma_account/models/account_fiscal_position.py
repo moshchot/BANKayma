f9a70c651149bf9c0470393d1f0a1acaa0055d1e
Tali Bo <19168053+toyrobot@users.noreply.github.com>
2024-10-29 21:35:56 +0000

    translatable vendor doc+ sumit label

--- bankayma_account/models/account_fiscal_position.py
+++ bankayma_account/models/account_fiscal_position.py
@@ -10,6 +10,7 @@ class AccountFiscalPosition(models.Model):
     code = fields.Char()
     vendor_doc_mandatory = fields.Boolean(
         "Mandatory document upload",
+        translate=True,
         help="Check this field to have vendors with this fiscal position "
         "upload a document for every invoice",
     )
