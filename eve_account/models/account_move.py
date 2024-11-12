44d041c8a678b5c86aae6633455547cf31bf7865
Holger Brunn <mail@hunki-enterprises.com>
2024-11-12 10:22:04 +0100

    [ADD] #745 eve_account: allow waiving overhead on a per-move basis

--- eve_account/models/account_move.py
+++ eve_account/models/account_move.py
@@ -1,11 +1,17 @@
 # Copyright 2024 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import _, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
+
+    bankayma_waive_overhead = fields.Boolean(
+        "Waive overhead",
+        help="Exceptionally don't charge overhead for this move",
+        states={"draft": [("readonly", False)], "posted": [("readonly", True)]},
+    )
 
     def _bankayma_invoice_child_income(
         self,
@@ -13,6 +19,14 @@ class AccountMove(models.Model):
         post=True,
         pay=True,
     ):
+        if self.bankayma_waive_overhead:
+            self.message_post(
+                body=_(
+                    "Overhead of %d%% was waived on this move",
+                    self.journal_id.bankayma_overhead_percentage,
+                )
+            )
+            return
         return super()._bankayma_invoice_child_income(
             fraction=self.journal_id.bankayma_overhead_percentage / 100,
             post=post,
