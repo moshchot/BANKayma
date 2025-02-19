7ce20c92db11595accdc7b50d2358fbf69c3c271
Tali Bo <19168053+toyrobot@users.noreply.github.com>
2025-02-19 20:48:19 +0000

    eve paid subject def

--- eve_account/models/account_move.py
+++ eve_account/models/account_move.py
@@ -42,3 +42,26 @@ class AccountMove(models.Model):
             post=post,
             pay=pay,
         )
+
+    def _invoice_paid_hook(self):
+        """Invoice overhead if necessary"""
+        for this in self.sudo().with_context(skip_invoice_sync=False):
+            if this.journal_id.company_cascade_parent_id.bankayma_charge_overhead:
+                parent_journal = this.journal_id.company_cascade_parent_id
+                this.with_company(this.company_id)._bankayma_invoice_child_income(
+                    fraction=parent_journal.bankayma_overhead_percentage / 100
+                )
+        for this in self:
+            if this.journal_id.bankayma_qweb_template_invoice_paid:
+                this.message_post_with_view(
+                    this.journal_id.bankayma_qweb_template_invoice_paid,
+                    subject=_(
+                        "[EVE] %(company)s you've been paid! (Ref %(ref)s)"
+                    )
+                    % dict(company=this.company_id.name, ref=this.name),
+                    message_type="comment",
+                    subtype_id=self.env.ref(
+                        "bankayma_account.message_subtype_vendor"
+                    ).id,
+                )
+        return super()._invoice_paid_hook()
