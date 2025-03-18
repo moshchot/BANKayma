8f05da7b335a0a87c905a2b9c5b42fa5fe3b9126
Holger Brunn <mail@hunki-enterprises.com>
2025-03-18 14:23:56 +0100

    [FIX] #835 handle editable inclusive taxes

--- bankayma_account/wizards/bankayma_move_edit_tax_totals.py
+++ bankayma_account/wizards/bankayma_move_edit_tax_totals.py
@@ -27,21 +27,58 @@ class BankaymaMoveEditTaxTotals(models.TransientModel):
         return result
 
     def action_edit_tax_totals(self):
-        diff = sum(self.mapped("line_ids.line_id.balance")) - sum(
-            self.mapped("line_ids.balance")
-        )
-        if not diff:
+        diff_included = {}
+        diff_excluded = 0.0
+        for line in self.line_ids:
+            if line.line_id.tax_line_id.price_include:
+                diff_included[line.line_id] = line.line_id.balance - line.balance
+            else:
+                diff_excluded += line.line_id.balance - line.balance
+        if not diff_included and not diff_excluded:
             return
+
         move = self.line_ids.line_id.move_id
+
+        product_lines = []
+        for tax_line, diff in diff_included.items():
+            sum_prices = move.currency_id.round(
+                sum(move.invoice_line_ids.mapped("price_unit"))
+            )
+            if not sum_prices:
+                continue
+            for product_line in move.invoice_line_ids.filtered(
+                lambda x: tax_line.tax_line_id in x.tax_ids
+            ):
+                weighted_diff = diff * product_line.price_unit / sum_prices
+                for _dummy, product_line_id, vals in product_lines:
+                    if product_line_id == product_line.id:
+                        vals["balance"] += weighted_diff
+                        vals["price_subtotal"] += weighted_diff
+                        break
+                else:
+                    product_lines.append(
+                        fields.Command.update(
+                            product_line.id,
+                            {
+                                "balance": product_line.balance + weighted_diff,
+                                "price_subtotal": product_line.price_subtotal
+                                + weighted_diff,
+                            },
+                        )
+                    )
+
         move.write(
             {
-                "line_ids": [
+                "line_ids": product_lines
+                + [
                     fields.Command.update(line.line_id.id, {"balance": line.balance})
                     for line in self.line_ids
                     if line.balance != line.line_id.balance
                 ]
                 + [
-                    fields.Command.update(line.id, {"balance": line.balance + diff})
+                    fields.Command.update(
+                        line.id, {"balance": line.balance + diff_excluded}
+                    )
                     for line in move.line_ids
                     if line.account_id.account_type
                     in ("liability_payable", "asset_receivable")
