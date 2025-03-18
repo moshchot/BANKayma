7d7f79a787cb9c7bb531ec87ffd15a8afbe3863c
Holger Brunn <mail@hunki-enterprises.com>
2025-03-18 16:13:05 +0100

    [ADD] #854 make account.analytic.account#balance available for
    bankayma_base.group_user

--- bankayma_account/models/account_analytic_account.py
+++ bankayma_account/models/account_analytic_account.py
@@ -8,6 +8,9 @@ class AccountAnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
 
     plan_id = fields.Many2one(default=lambda self: self._default_analytic_plan_id())
+    balance = fields.Monetary(
+        groups="account.group_account_readonly,bankayma_base.group_user"
+    )
 
     def _default_analytic_plan_id(self):
         return self.env["account.analytic.plan"].search(
