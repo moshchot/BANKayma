f58b705a4b3856da3ef7671fb315b86029cca75f
Holger Brunn <mail@hunki-enterprises.com>
2025-04-15 07:43:44 +0200

    [ADD] #872 allow any fixed date payment terms for org managers

--- eve_account/models/account_move.py
+++ eve_account/models/account_move.py
@@ -8,8 +8,16 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     invoice_payment_term_id = fields.Many2one(
-        domain="['|', ('fixed_date', '=', False), ('fixed_date', '>', current_date)]"
+        domain=lambda self: self._domain_invoice_payment_term_id()
     )
+
+    def _domain_invoice_payment_term_id(self):
+        """Generate group specific domain for invoice_payment_term_id"""
+        return (
+            "['|', ('fixed_date', '=', False), ('fixed_date', '>', current_date)]"
+            if not self.env.user.has_group("bankayma_base.group_org_manager")
+            else "[]"
+        )
 
     def _bankayma_invoice_child_income_get_parent_company(self):
         """Get the parent that invoices overhead"""
