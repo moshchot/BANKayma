8134d0e131052ba5609327941860986892a2efcf
Holger Brunn <mail@hunki-enterprises.com>
2025-05-20 13:49:57 +0200

    [FIX] #893 handle updating analytic distributions on posted moves
    with manually edited taxes correctly.
    recreate previously wrongly computed analytic lines

--- bankayma_account/wizards/account_move_update_analytic.py
+++ bankayma_account/wizards/account_move_update_analytic.py
@@ -1,7 +1,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import _, exceptions, models
 from odoo.fields import Command
+from odoo.tools import frozendict
 
 from odoo.addons.account_move_update_analytic.models.account_move_line import (
     force_state_sentinel,
@@ -12,36 +13,66 @@ class AccountMoveUpdateAnalytic(models.TransientModel):
     _inherit = "account.move.update.analytic.wizard"
 
     def update_analytic_lines(self):
+        # super will recompute taxes, so we need to save the balances of
+        # potentially changed taxes and impose those values after the tax
+        # recomputation happened. as the original tax line(s) have been
+        # deleted, we need to match the new tax line by tax key, but without
+        # considering the analytic distribution as we're about to change that
+
+        move = self.line_id.move_id
+
+        def tax_key(line):
+            return frozendict(line.tax_key, analytic_distribution=False)
+
         custom_taxes_wizard = (
             self.env["bankayma.move.edit.tax.totals"]
             .with_context(
-                active_ids=self.line_id.move_id.ids,
-                account_move_update_analytic=force_state_sentinel,
+                active_ids=move.ids,
             )
             .sudo()
             .create({})
         )
-        tax2amount = {
-            line.line_id.tax_line_id: line.balance
-            for line in custom_taxes_wizard.line_ids
+        taxkey2balance = {
+            tax_key(line.line_id): line.balance for line in custom_taxes_wizard.line_ids
         }
-        custom_taxes_wizard.line_ids = False
+        ml2balance = {line: line.balance for line in move.line_ids}
+
+        if move.state == "posted":
+            for line in move.line_ids:
+                if any(
+                    line2
+                    for line2 in move.line_ids
+                    if line2 != line and tax_key(line) == tax_key(line2)
+                ):
+                    raise exceptions.UserError(
+                        _(
+                            "This move is too complex for inline changing. "
+                            "Revert it and edit a copy"
+                        )
+                    )
 
         result = super().update_analytic_lines()
 
-        custom_taxes_wizard.line_ids = [
-            Command.create(
-                {
-                    "line_id": self.line_id.move_id.line_ids.filtered(
-                        lambda x: x.tax_line_id == tax
-                    )[:1].id,
-                    "balance": balance,
-                }
-            )
-            for tax, balance in tax2amount.items()
-            if tax in self.line_id.move_id.line_ids.tax_line_id
+        changed_move_lines = [
+            Command.update(line.id, {"balance": balance})
+            for line, balance in ml2balance.items()
+            if line.exists()
+        ] + [
+            Command.update(line.id, {"balance": taxkey2balance[tax_key(line)]})
+            for line in move.line_ids
+            if tax_key(line) in taxkey2balance
         ]
 
-        custom_taxes_wizard.action_edit_tax_totals()
+        if changed_move_lines:
+            move.with_context(
+                check_move_validity=False,
+                account_move_update_analytic=force_state_sentinel,
+            ).write(
+                {
+                    "line_ids": changed_move_lines,
+                }
+            )
+            move.line_ids.analytic_line_ids.unlink()
+            move.line_ids._create_analytic_lines()
 
         return result
