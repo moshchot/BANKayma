9f80c6b040e06a72b3bbff38566948f8f0c42f4d
Holger Brunn <mail@hunki-enterprises.com>
2025-06-10 15:11:24 +0200

    [ADD] #871 pass payment wizard's comment to sumit

--- bankayma_account/models/__init__.py
+++ bankayma_account/models/__init__.py
@@ -7,6 +7,7 @@ from . import account_journal
 from . import account_move
 from . import account_move_line
 from . import account_payment_method
+from . import account_payment
 from . import account_tax
 from . import account_tax_group
 from . import mail_template
