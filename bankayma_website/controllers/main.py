32da32194b1f15d785f159586ef0fdbcffd34b9b
Holger Brunn <mail@hunki-enterprises.com>
2025-07-29 16:29:33 +0200

    fixup! [ADD] #955 more project page content

--- bankayma_website/controllers/main.py
+++ bankayma_website/controllers/main.py
@@ -1,4 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from lxml import html
+from markupsafe import Markup
+
 from odoo import http
 
 
@@ -7,5 +10,33 @@ class CompaniesController(http.Controller):
     def render_company_page(self, company):
         return http.Response(
             template="bankayma_website.company_page",
-            qcontext={"object": company.sudo()},
+            qcontext={
+                "object": company.sudo(),
+                "render_dynamic_snippet": self._render_dynamic_snippet,
+            },
         )
+
+    def _render_dynamic_snippet(self, xmlid, records):
+        data = (
+            http.request.env["website.snippet.filter"]
+            .new(
+                {
+                    "model_name": records._name,
+                    "field_names": "display_name",
+                }
+            )
+            ._filter_records_to_values(records)
+        )
+        xml = (
+            http.request.env["ir.qweb"]
+            .with_context(inherit_branding=False)
+            ._render(xmlid, {"records": data})
+        )
+        output = b'<div class="row my-4">'
+        for node in html.fromstring(xml).getchildren():
+            output += (
+                b'<div class="d-flex flex-grow-0 flex-shrink-0 col-3">%s</div>'
+                % html.tostring(node)
+            )
+        output += b"</div>"
+        return Markup(output.decode("utf8"))
