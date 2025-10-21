abf58a8e09871d0a861e8143d7291372a2668cda
Holger Brunn <mail@hunki-enterprises.com>
2025-10-21 09:15:38 +0200

    [IMP] #985 place sticky search bar below sticky menu

---
+++ bankayma_website/static/src/components/fixed_header.esm.js
@@ -0,0 +1,16 @@
+/** @odoo-module **/
+import {extraMenuUpdateCallbacks} from "website.content.menu";
+
+function resetStickyTop() {
+    const $header = jQuery(".o_header_fixed.o_top_fixed_element");
+    const $search = jQuery(".bankayma_event_searchbar,.bankayma_searchbar");
+    if ($header.length && $search.length) {
+        if ($header.hasClass("o_header_is_scrolled")) {
+            $search.css("padding-top", `${$header.outerHeight()}px`);
+        } else {
+            $search.css("padding-top", "");
+        }
+    }
+}
+
+extraMenuUpdateCallbacks.push(resetStickyTop);
