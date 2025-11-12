e9d602bffc01ffec34dd71e197af485559bbb4b9
Holger Brunn <mail@hunki-enterprises.com>
2025-11-12 07:07:19 +0100

    [IMP] more migration code

--- bankayma_migrate_v18/models/operating_unit.py
+++ bankayma_migrate_v18/models/operating_unit.py
@@ -5,4 +5,5 @@ from odoo import fields, models
 class OperatingUnit(models.Model):
     _inherit = "operating.unit"
 
-    bankayma_from_company_id = fields.Many2one("res.company")
+    bankayma_from_company_id = fields.Integer()
+    parent_id = fields.Many2one("operating.unit")
