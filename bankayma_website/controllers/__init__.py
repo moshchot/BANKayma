bbc5880eaaf4765cb01790a25d9ea16328aeec0e
Holger Brunn <mail@hunki-enterprises.com>
2025-12-16 15:48:48 +0100

    [IMP] #1009 change path of blog posts

--- bankayma_website/controllers/__init__.py
+++ bankayma_website/controllers/__init__.py
@@ -1,3 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import main
+from . import website_blog
 from . import website_event
--- bankayma_website/models/blog_post.py
+++ bankayma_website/models/blog_post.py
@@ -1,8 +1,14 @@
 from odoo import fields, models
 
+from odoo.addons.http_routing.models.ir_http import slug
+
 
 class BlogPost(models.Model):
     _inherit = ["blog.post", "bankayma.search.drop.company.mixin"]
     _name = "blog.post"
 
     company_id = fields.Many2one("res.company", default=lambda self: self.env.company)
+
+    def _compute_website_url(self):
+        for this in self:
+            this.website_url = "/news/%s" % slug(this)
