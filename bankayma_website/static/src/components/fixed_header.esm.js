33f3cc67b280c11ff94d79c8a494d070dd1ceae5
Holger Brunn <mail@hunki-enterprises.com>
2025-12-17 15:52:36 +0100

    [IMP] #1009 make product searchbar stick on top

--- bankayma_website/static/src/components/fixed_header.esm.js
+++ bankayma_website/static/src/components/fixed_header.esm.js
@@ -3,7 +3,9 @@ import {extraMenuUpdateCallbacks} from "website.content.menu";
 
 function resetStickyTop() {
     const $header = jQuery(".o_header_fixed.o_top_fixed_element");
-    const $search = jQuery(".bankayma_event_searchbar,.bankayma_searchbar");
+    const $search = jQuery(
+        ".bankayma_event_searchbar,.bankayma_searchbar,div.products_header"
+    );
     if ($header.length && $search.length) {
         if ($header.hasClass("o_header_is_scrolled")) {
             $search.css("padding-top", `${$header.outerHeight()}px`);
