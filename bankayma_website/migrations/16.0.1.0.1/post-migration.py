c06bafca2e9a30d5e483b97fe652f66cfc17b2f8
Holger Brunn <mail@hunki-enterprises.com>
2026-01-06 13:30:47 +0100

    [IMP] #1014 remove mass mailing user group from all but org managers

--- bankayma_website/migrations/16.0.1.0.1/post-migration.py
+++ bankayma_website/migrations/16.0.1.0.1/post-migration.py
@@ -7,6 +7,7 @@ from odoo.fields import Command
 def migrate(env, version=None):
     event_manager_group = env.ref("event.group_event_manager")
     mail_template_editor_group = env.ref("mail.group_mail_template_editor")
+    mass_mailing_user_group = env.ref("mass_mailing.group_mass_mailing_user")
 
     env.ref("bankayma_website.group_website").write(
         {
@@ -21,11 +22,18 @@ def migrate(env, version=None):
     env.ref("base.default_user").write(
         {"groups_id": [Command.unlink(event_manager_group.id)]}
     )
+    (
+        env.ref("bankayma_base.group_user") + env.ref("bankayma_base.group_manager")
+    ).write({"implied_ids": [Command.unlink(mass_mailing_user_group.id)]})
 
-    for group in event_manager_group + mail_template_editor_group:
+    for group in (
+        event_manager_group + mail_template_editor_group + mass_mailing_user_group
+    ):
         for user in env["res.users"].search([("groups_id", "=", group.id)]):
-            if user.has_group("bankayma_base.group_full") or user.has_group(
-                "bankayma_base.group_org_manager"
+            if (
+                user.has_group("bankayma_base.group_full")
+                or user.has_group("bankayma_base.group_org_manager")
+                or user.has_group("base.group_settings")
             ):
                 continue
             user.write({"groups_id": [Command.unlink(group.id)]})
