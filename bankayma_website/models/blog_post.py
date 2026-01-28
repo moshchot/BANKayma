7745500832318e34bab9912964637a5e80f260c7
Holger Brunn <mail@hunki-enterprises.com>
2026-01-28 17:40:52 +0100

    [IMP] #1011 add projects restriction to blog controller

--- bankayma_website/models/blog_post.py
+++ bankayma_website/models/blog_post.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+import json
+
+from odoo import api, fields, http, models
 
 from odoo.addons.http_routing.models.ir_http import slug
 
@@ -12,3 +14,12 @@ class BlogPost(models.Model):
     def _compute_website_url(self):
         for this in self:
             this.website_url = "/news/%s" % slug(this)
+
+    @api.model
+    def _search_build_domain(self, domain_list, search, fields, extra=None):
+        result = super()._search_build_domain(domain_list, search, fields, extra=extra)
+        if http.request and http.request.params.get("projects"):
+            result.append(
+                ("company_id", "in", json.loads(http.request.params["projects"]))
+            )
+        return result
