20fe11171f8277c02f8bdde7f65c65c102524799
Holger Brunn <mail@hunki-enterprises.com>
2026-02-17 10:51:58 +0100

    [FIX] #1055 migration script to set page names; code to guarantee name

--- bankayma_website/models/website_page.py
+++ bankayma_website/models/website_page.py
@@ -1,7 +1,15 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class WebsitePage(models.Model):
     _inherit = "website.page"
 
     name = fields.Char(translate=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        result = super().create(vals_list)
+        for this in result:
+            if not this.name and this.view_id.name:
+                this.name = this.view_id.name
+        return result
