a42ec0571d8295468c8a2521e25b37debcb0c63a
Holger Brunn <mail@hunki-enterprises.com>
2026-02-18 17:54:27 +0100

    [ADD] #1049 bankayma_website_sale_loyalty

---
+++ bankayma_website_sale_loyalty/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "BANKayma (website_sale_loyality)",
+    "summary": "BANKayma website_sale_loyality customizations",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "author": "Moshchot Coop",
+    "license": "AGPL-3",
+    "application": True,
+    "depends": [
+        "bankayma_website_sale",
+        "website_sale_loyalty",
+    ],
+    "data": [
+        "views/loyalty_reward.xml",
+    ],
+    "demo": [],
+    "website": "https://github.com/moshchot/BANKayma",
+    "external_dependencies": {"python": []},
+}
