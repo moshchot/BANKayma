773ba50e96589c31707a2a479811cb000b3e796f
Holger Brunn <mail@hunki-enterprises.com>
2026-02-24 20:31:24 +0100

    [IMP] #1063 random product snippet filter

--- bankayma_website_sale/models/website_snippet_filter.py
+++ bankayma_website_sale/models/website_snippet_filter.py
@@ -1,3 +1,5 @@
+import random
+
 from odoo import models
 
 from odoo.addons.website.models import ir_http
@@ -11,3 +13,11 @@ class WebsiteSnippetFilter(models.Model):
         if records._name in ("product.product", "product.template") and website:
             records = records.filtered_domain(website.sale_product_domain())
         return super()._filter_records_to_values(records, is_sample=is_sample)
+
+    def _get_products_random(self, website, limit, domain, context):
+        products = (
+            self.env["product.product"]
+            .with_context(display_default_code=False, add2cart_rerender=False)
+            .search(domain)
+        )
+        return products.browse(random.sample(products.ids, len(products)))[:limit]
