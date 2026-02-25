f7213de1acfe9c0f6f94c7d299a772d02b5afd98
Holger Brunn <mail@hunki-enterprises.com>
2026-02-25 10:20:54 +0100

    [FIX] #1057 scrolling header, search bar width

--- bankayma_website/static/src/components/fixed_header.esm.js
+++ bankayma_website/static/src/components/fixed_header.esm.js
@@ -2,15 +2,18 @@
 import {extraMenuUpdateCallbacks} from "website.content.menu";
 
 function resetStickyTop() {
-    const $header = jQuery(".o_header_fixed.o_top_fixed_element");
+    const $header = jQuery("#top");
     const $search = jQuery(
         ".bankayma_event_searchbar,.bankayma_searchbar,div.products_header"
     );
     if ($header.length && $search.length) {
         if ($header.hasClass("o_header_is_scrolled")) {
-            $search.css("padding-top", `${$header.outerHeight()}px`);
+            $search.css(
+                "transform",
+                `translate(0px, ${$header.outerHeight() + $header.position().top}px)`
+            );
         } else {
-            $search.css("padding-top", "");
+            $search.attr("style", null);
         }
     }
 }
