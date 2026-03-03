1111a3e4b0a597994dc9af3b58010eed604aad5a
Holger Brunn <mail@hunki-enterprises.com>
2026-03-03 06:43:32 +0100

    [IMP] #1067 allow payment in installments from a configurable threshold

--- l10n_il_sumit/models/payment_provider.py
+++ l10n_il_sumit/models/payment_provider.py
@@ -11,6 +11,14 @@ class PaymentProvider(models.Model):
         selection_add=[("sumit", "Sumit")], ondelete={"sumit": "cascade"}
     )
     sumit_account_id = fields.Many2one("sumit.account", required_if_provider="sumit")
+    sumit_installment_threshold = fields.Monetary(
+        "Threshold for installments",
+        currency_field="main_currency_id",
+        help="If a transaction amount is above this value, it may be paid in installments",
+    )
+    sumit_installment_count = fields.Integer(
+        "Maximum installments", help="The maximum amount of installments to allow"
+    )
 
     def _should_build_inline_form(self, is_validation=False):
         if self.code == "sumit":
